526e93d21dafb36699d3c9be54216bf94a4c1bc7
Robert Stein <robert@blueshoe.de>
2021-04-02 17:10:01 +0200

    refactor: avg response time requirement

--- locustfile.py
+++ locustfile.py
@@ -50,8 +50,8 @@ def _(environment, **kw):
     if environment.stats.total.fail_ratio > 0.01:
         logging.error("Test failed due to failure ratio > 1%")
         environment.process_exit_code = 1
-    elif environment.stats.total.avg_response_time > 6:
-        logging.error("Test failed due to average response time ratio > 3 ms")
+    elif environment.stats.total.avg_response_time > 4:
+        logging.error("Test failed due to average response time ratio > 4 ms")
         environment.process_exit_code = 1
     elif environment.stats.total.get_response_time_percentile(0.95) > 6:
         logging.error("Test failed due to 95th percentile response time > 6 ms")
