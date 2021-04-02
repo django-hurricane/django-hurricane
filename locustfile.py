cd261b0670400dfcd71f011537357456ce49689b
Robert Stein <robert@blueshoe.de>
2021-04-02 16:49:39 +0200

    feature: add load testing to github actions

--- locustfile.py
+++ locustfile.py
@@ -4,6 +4,8 @@ from locust import HttpUser, between, events, task
 from locust.contrib.fasthttp import FastHttpUser
 
 
+class WebsiteUser(FastHttpUser):
+
 class HurricaneWebsiteUser(FastHttpUser):
     wait_time = between(5, 15)
     url = "/"
@@ -36,6 +38,23 @@ def _(environment, **kw):
         environment.process_exit_code = 1
     elif environment.stats.total.get_response_time_percentile(0.95) > 800:
         logging.error("Test failed due to 95th percentile response time > 800 ms")
+        environment.process_exit_code = 1
+    else:
+        environment.process_exit_code = 0
+
+
+@events.quitting.add_listener
+def _(environment, **kw):
+    logging.info(f"Average response time: {environment.stats.total.avg_response_time}")
+    logging.info(f"95th percentile response time: {environment.stats.total.get_response_time_percentile(0.95)}")
+    if environment.stats.total.fail_ratio > 0.01:
+        logging.error("Test failed due to failure ratio > 1%")
+        environment.process_exit_code = 1
+    elif environment.stats.total.avg_response_time > 6:
+        logging.error("Test failed due to average response time ratio > 3 ms")
+        environment.process_exit_code = 1
+    elif environment.stats.total.get_response_time_percentile(0.95) > 6:
+        logging.error("Test failed due to 95th percentile response time > 2 ms")
         environment.process_exit_code = 1
     else:
         environment.process_exit_code = 0
