9920e836bfb53bcdd95a188ad294c60a0ddb9d8f
Robert Stein <robert@blueshoe.de>
2021-04-02 16:49:39 +0200

    feature: add load testing to github actions

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,29 @@
-from locust import HttpUser, between, task
+import logging
+
+from locust import HttpUser, between, events, task
+from locust.contrib.fasthttp import FastHttpUser
 
 
-class WebsiteUser(HttpUser):
+class WebsiteUser(FastHttpUser):
     wait_time = between(5, 15)
 
     @task
     def admin(self):
         self.client.get("/")
+
+
+@events.quitting.add_listener
+def _(environment, **kw):
+    logging.info(f"Average response time: {environment.stats.total.avg_response_time}")
+    logging.info(f"95th percentile response time: {environment.stats.total.get_response_time_percentile(0.95)}")
+    if environment.stats.total.fail_ratio > 0.01:
+        logging.error("Test failed due to failure ratio > 1%")
+        environment.process_exit_code = 1
+    elif environment.stats.total.avg_response_time > 6:
+        logging.error("Test failed due to average response time ratio > 3 ms")
+        environment.process_exit_code = 1
+    elif environment.stats.total.get_response_time_percentile(0.95) > 6:
+        logging.error("Test failed due to 95th percentile response time > 2 ms")
+        environment.process_exit_code = 1
+    else:
+        environment.process_exit_code = 0
