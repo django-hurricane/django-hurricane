2e0948593b2b1b749f4207b48c638887f2b74379
Robert Stein <robert@blueshoe.de>
2021-04-06 21:50:17 +0200

    refactor: add heavy and mid heavy view to load test

--- locustfile.py
+++ locustfile.py
@@ -4,8 +4,6 @@ from locust import HttpUser, between, events, task
 from locust.contrib.fasthttp import FastHttpUser
 
 
-class WebsiteUser(FastHttpUser):
-
 class HurricaneWebsiteUser(FastHttpUser):
     wait_time = between(5, 15)
     url = "/"
@@ -38,23 +36,6 @@ def _(environment, **kw):
         environment.process_exit_code = 1
     elif environment.stats.total.get_response_time_percentile(0.95) > 800:
         logging.error("Test failed due to 95th percentile response time > 800 ms")
-        environment.process_exit_code = 1
-    else:
-        environment.process_exit_code = 0
-
-
-@events.quitting.add_listener
-def _(environment, **kw):
-    logging.info(f"Average response time: {environment.stats.total.avg_response_time}")
-    logging.info(f"95th percentile response time: {environment.stats.total.get_response_time_percentile(0.95)}")
-    if environment.stats.total.fail_ratio > 0.01:
-        logging.error("Test failed due to failure ratio > 1%")
-        environment.process_exit_code = 1
-    elif environment.stats.total.avg_response_time > 4:
-        logging.error("Test failed due to average response time ratio > 4 ms")
-        environment.process_exit_code = 1
-    elif environment.stats.total.get_response_time_percentile(0.95) > 6:
-        logging.error("Test failed due to 95th percentile response time > 6 ms")
         environment.process_exit_code = 1
     else:
         environment.process_exit_code = 0
