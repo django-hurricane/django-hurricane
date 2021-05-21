127836b5e29c31af85144d830dbaa128d6b42665
Robert Stein <SteinRobert@users.noreply.github.com>
2021-05-21 22:54:42 +0200

    tests: add load testing to github actions (#20)

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,41 @@
-from locust import HttpUser, between, task
+import logging
+
+from locust import HttpUser, between, events, task
+from locust.contrib.fasthttp import FastHttpUser
 
 
-class WebsiteUser(HttpUser):
+class HurricaneWebsiteUser(FastHttpUser):
     wait_time = between(5, 15)
+    url = "/"
+    weight = 1
 
     @task
-    def admin(self):
-        self.client.get("/")
+    def request(self):
+        self.client.get(self.url)
+
+
+class MediumHurricaneWebsiteUser(HurricaneWebsiteUser):
+    url = "/medium"
+    weight = 3
+
+
+class HeavyHurricaneWebsiteUser(HurricaneWebsiteUser):
+    url = "/heavy"
+    weight = 1
+
+
+@events.quitting.add_listener
+def _(environment, **kw):
+    logging.info(f"Average response time: {environment.stats.total.avg_response_time}")
+    logging.info(f"95th percentile response time: {environment.stats.total.get_response_time_percentile(0.95)}")
+    if environment.stats.total.fail_ratio > 0.025:
+        logging.error("Test failed due to failure ratio > 2.5%")
+        environment.process_exit_code = 1
+    elif environment.stats.total.avg_response_time > 300:
+        logging.error("Test failed due to average response time ratio > 300 ms")
+        environment.process_exit_code = 1
+    elif environment.stats.total.get_response_time_percentile(0.95) > 950:
+        logging.error("Test failed due to 95th percentile response time > 900 ms")
+        environment.process_exit_code = 1
+    else:
+        environment.process_exit_code = 0
--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,3 +1,5 @@
+import time
+
 from django.http import HttpResponse
 from django.urls import path
 
@@ -6,4 +8,18 @@ def test_view(request):
     return HttpResponse("Hello world!", status=200)
 
 
-urlpatterns = [path("", test_view)]
+def medium_view(request):
+    time.sleep(0.1)
+    return HttpResponse("Hello world!", status=200)
+
+
+def heavy_view(request):
+    time.sleep(0.5)
+    return HttpResponse("Hello world!", status=200)
+
+
+urlpatterns = [
+    path("", test_view),
+    path("medium", medium_view),
+    path("heavy", heavy_view),
+]
