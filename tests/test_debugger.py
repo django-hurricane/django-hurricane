e69e0447e8d9308658d94b9dd5067dfeec76363e
vvvityaaa <vstudenjak@gmail.com>
2022-04-15 12:05:50 +0200

    test: debugger import errors

--- tests/test_debugger.py
+++ tests/test_debugger.py
@@ -11,3 +11,21 @@ class HurricanDebuggerServerTest(HurricanServerTest):
         out, err = self.driver.get_output(read_all=True)
         self.assertEqual(res.status, 200)
         self.assertIn("Listening for debug clients at port 5678", out)
+
+    def test_debugger_import_debugpy(self):
+        import sys
+
+        from hurricane.server.debugging import setup_debugpy
+
+        sys.modules["debugpy"] = None
+        options = {"debugger": True}
+        setup_debugpy(options)
+
+    def test_pycharm_import_pydevd_pycharm(self):
+        import sys
+
+        from hurricane.server.debugging import setup_pycharm
+
+        sys.modules["pydevd_pycharm"] = None
+        options = {"pycharm_host": True}
+        setup_pycharm(options)
