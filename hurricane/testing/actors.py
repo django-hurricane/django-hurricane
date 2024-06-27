69474fae94d356f9cd224aea098cdea188f200c1
liquidiert <haberederkorbinian@googlemail.com>
2024-06-27 09:39:58 +0200

    feat: add tests for huge file uploads

--- hurricane/testing/actors.py
+++ hurricane/testing/actors.py
@@ -1,5 +1,7 @@
+import os
 import http
 import json
+import mimetypes
 
 import pika
 import tornado.ioloop
@@ -32,7 +34,33 @@ class HTTPClient(object):
         res = self.conn.getresponse()
         datas = res.read()  # type: bytes
         return self.Response({"status": res.status, "text": datas.decode("utf-8")})
+    
+    def post_file(self, path: str, file_path: str) -> Response:
+        boundary = '----WebKitFormBoundary7MA4YWxkTrZu0gW'
+        filename = os.path.basename(file_path)
+        content_type, _ = mimetypes.guess_type(file_path)
 
+        with open(file_path, 'rb') as f:
+            file_content = f.read()
+
+        body = (
+            f'--{boundary}\r\n'
+            f'Content-Disposition: form-data; name="file"; filename="{filename}"\r\n'
+            f'Content-Type: {content_type}\r\n\r\n'
+            f'{file_content.decode("latin1")}\r\n'
+            f'--{boundary}--\r\n'
+        )
+
+        headers = {
+            'Content-Type': f'multipart/form-data; boundary={boundary}',
+            'Content-Length': str(len(body))
+        }
+
+        self.conn.request("POST", path, body.encode("latin1"), headers)
+        res = self.conn.getresponse()
+        data = res.read()
+
+        return self.Response({"status": res.status, "text": data.decode("utf-8")})
 
 class TestPublisher(object):
     def __init__(self, host, port, vhost="/"):
